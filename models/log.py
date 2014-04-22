a7c37bff04fdaa9a688f9ddb2a2a94ddf941100c
Shay Erlichmen <erlichmen@gmail.com>
2014-04-22 14:48:28 +0300

    add more data to the logs entry

--- models/log.py
+++ models/log.py
@@ -1,5 +1,7 @@
 from google.appengine.ext import ndb
 from google.appengine.api import blobstore
+import urllib
+import hashlib
 
 
 class Log(ndb.Model):
@@ -8,6 +10,29 @@ class Log(ndb.Model):
     body = ndb.StringProperty(required=True)
     salt = ndb.StringProperty(required=True)
     links = ndb.StringProperty(repeated=True)
+
+    @property
+    def from_user_params(self):
+        import email.utils
+        return email.utils.parseaddr(self.from_user)
+
+    @property
+    def from_user_display_name(self):
+        return self.from_user_params[0]
+
+    @property
+    def from_user_email(self):
+        return self.from_user_params[1]
+
+    def gravatar_url(self, size=None):
+        email = self.from_user_email.lower()
+        default = "http://www.example.com/default.jpg"
+        size = size or 32
+
+        gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+        gravatar_url += urllib.urlencode({'d': default, 's': str(size)})
+
+        return gravatar_url
 
     @property
     def actual_links(self):
